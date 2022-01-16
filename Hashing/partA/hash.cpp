7f429770e503c2ffc116fdbd0868b25f3d7d29e6
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-01-16 11:30:00 +0500

    Update and rename Hashing.cpp to Hashing/partA/hash.cpp

---
+++ Hashing/partA/hash.cpp
@@ -0,0 +1,133 @@
+
+#include<iostream>
+#include <List>
+#include <ctime>
+#include "HashTable.h"
+using namespace std;
+
+ll prime = pow(2, 61) - 1;
+ll p = prime - 1;
+ll a = rand() % p + 1;
+ll b = rand() % prime;
+ll m = 3;
+hashTable table;
+
+//returns the array of n distinct integers in range [1,2^w] generated randomly
+ll* generateRandomKeys(ll n)
+{
+	ll* arr = new ll[n];
+	srand(time(0));
+	for (ll i = 0; i < n; i++)
+	{
+		ll x = pow(2, 64) - 1;
+		ll k = rand() % x + 1;
+		arr[i] = k;
+	}
+	return arr;
+}
+//takes a key value and hash it according to universal hash family: h(k) = ((ak + b) mod p) mod m
+ll hashThisK(ll k, ll m)
+{
+	ll hk = (((a * k) + b) % prime) % m;
+	return hk;
+}
+
+ll hashIt(ll* array,ll n)
+{
+	m = 3;
+	table.setParms(a, b, prime);
+	for (ll i = 0; i < n; i++)
+	{
+		//cout << i;
+		int op = (rand() % 3)+1;
+		if (op == 1)
+		{
+			//insert
+			if (table.insert(hashThisK(array[i], m), array[i])) //indexOfInsertion,valueOfKey
+			{
+				cout << "resized" << endl;
+			}
+
+		}
+		else if (op == 2)
+		{
+			//delete
+			if (table.currCount() > 0)
+			{
+				int d = rand() % n;
+				if (table.deleteKey(hashThisK(array[d], m), array[d])) //indexOfDeletion,valueOfKey
+				{
+					cout << "resized" << endl;
+				}
+			}
+			else
+			{
+				cout << "Hash table has no values to delete" << endl;
+			}
+		}
+		else
+		{
+			//search
+			if (table.currCount() > 0)
+			{
+				int d = rand() % n;
+				table.searchVal(array[d]);
+			}
+			else
+			{
+				cout << "Hash table has no values to Search" << endl;
+			}
+		}
+	}
+	table.printarr();
+	return 0;
+}
+int main()
+{
+	ll n;
+	cout << "enter an integer n:";
+	cin >> n;
+	ll *arr = new ll[n];
+	arr = generateRandomKeys(n);
+	//cout << "Keys are::" << endl;
+	for (ll i = 0; i < n; i++)
+	{
+		//cout << arr[i] << "\t";
+	}
+	cout<< endl;
+	hashIt(arr, n);
+	//int c = hash_a(5000);
+	/*hashTable<ll> a;
+	for (int i = 0; i < 100; i++)
+	{
+		a.insert(i);
+		cout << "we are inseting " << i << " while size is " << a.getSize() << endl;
+	}
+	for (int i = 0; i < 100; i++)
+	{
+		a.deleteKey();
+		cout << "we are deleting " << 100-i << " while size is " << a.getSize() << endl;
+	}*/
+	return 0;
+}
+
+
+//do we need to change a b p each time?
+//count parameter 1 for each index? or the linked list size for each?
+//how we know it is correct or not?
+
+
+/*
+int getM(int n)
+{
+	m = 3;
+	cout << "n is" << n << endl;
+	for (int i = 0; m < n; i++)
+	{
+		m = m * 2;
+		cout << "m is " << m << endl;
+	}
+	cout << m;
+	return m;
+}
+*/
