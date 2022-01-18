47273304c847887a95824d8266ebc8b26635cb4b
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-01-18 23:51:59 +0500

    Add files via upload

---
+++ Hashing/C#/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace PerfectHashing
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rnd = new Random();
+			ulong prime = (ulong)Math.Pow(2, 61) - 1;
+			ulong p = prime - 1;
+			ulong a = (ulong)rnd.Next() % p + 1;
+			ulong b = (ulong)rnd.Next() % prime;
+			ulong n = 5000;
+			pHashing[] table = new pHashing[n];
+			for(ulong i = 0; i < n; i++)
+            {
+				table[i] = new pHashing(n);
+            }
+			//int xx = 0;
+			ulong[] value = new ulong[n];
+			for (ulong j = 0; j < n; j++)
+			{
+				value[j] = (ulong)rnd.Next();
+				ulong index = hashThisK(value[j], n, a, b, prime);
+				table[index].insert(index, value[j]);
+			}
+			ulong x = 0;
+			for (ulong j = 0; j < n; j++)
+			{
+				Console.Write(j + "::");
+				//x += table[j].printarr();
+				ulong index = hashThisK(value[j], n, a, b, prime);
+				table[index].searchVal(value[j]);
+			}
+			Console.WriteLine(x);
+		}
+		public static ulong hashThisK(ulong k, ulong m, ulong a, ulong b, ulong prime)
+		{
+			ulong hk = (((a * k) + b) % prime) % m;
+			return hk;
+		}
+
+	}
+}
