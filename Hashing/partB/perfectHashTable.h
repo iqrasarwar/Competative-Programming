c78e69a7d9fcab2e62ef277f1d7acf580bb55b2e
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-01-18 18:31:34 +0500

    Add files via upload

--- Hashing/partB/perfectHashTable.h
+++ Hashing/partB/perfectHashTable.h
@@ -4,7 +4,7 @@
 #include <stack>
 using namespace std;
 #define ll unsigned long long int
-const ll S = 100;
+const ll S = 5000;
 
 class perfectHashTable
 {
@@ -44,21 +44,43 @@ void perfectHashTable::insert(ll index, ll value)
     }
     else
     {
-        cout << "collision for " << value << " with a " << a << " b " << b << endl;
-        a = rand() % p + 1;
-        b = rand() % prime;
+        bool avoided = false;
         ll* temp = new ll[S];
-        for (int i = 0; i < S;  i++)
+        int colliding = arr[newIndex];
+        cout << "for index "<<newIndex<<" collision of " << value << " and " << colliding << " with a " << a << " b " << b << endl;
+        for (int i = 0; i < S; i++)
         {
-            temp[i] = arr[i];
+            if (i != newIndex)
+                temp[i] = arr[i];
+            else
+                temp[i] = 0;
             arr[i] = 0;
+        }
+        while (avoided == false)
+        {
+            a = rand() % p + 1;
+            b = rand() % prime;
+            int thisIndex = hashThis(colliding);
+            int thisValue = hashThis(value);
+            if (thisIndex == thisValue)
+            {
+                cout<< "for index " << thisIndex << " still collding with a " << a << " b " << b << endl;
+                continue;
+            }
+            else
+            {
+                cout << "collision resolved!" << endl;
+                arr[thisIndex] = colliding;
+                arr[thisValue] = value;
+                avoided = true;
+            }
         }
         for (int i = 0; i < S; i++)
         {
             if (temp[i] != 0)
                 insert(index, temp[i]);
         }
-        insert(index, value);
+        cout << "totally collision resolved!" << endl;
     }
 }
 
