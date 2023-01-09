d1f27e9cac8db085056eab5774789640422078f4
Iqra Sarwar <71145709+iqrasarwar@users.noreply.github.com>
2023-01-09 23:07:59 +0500

    Create 1484. Group Sold Products By The Date.sql

---
+++ LeetCode/SQL/1484. Group Sold Products By The Date.sql
@@ -0,0 +1,1 @@
+# Write your MySQL query statement below select sell_date,COUNT(DISTINCT product) as num_sold, GROUP_CONCAT(DISTINCT product) AS products from Activities group by sell_date order by sell_date
